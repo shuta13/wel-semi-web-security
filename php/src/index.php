ac1a425bd96ad6228d5bc95bb61c4c8f6813ab32
shuta13 <hollystarsun@gmail.com>
2020-05-24 18:39:57 +0900

    fix

--- php/src/index.php
+++ php/src/index.php
@@ -1,6 +1,3 @@
-<?php
-declare(strict_types = 1);
-?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,41 +5,9 @@ declare(strict_types = 1);
     <title>welcome-seminar-2020</title>
   </head>
   <body>
-  <table border="1">
-  <tr>
-    <th>id</th>
-    <th>名前</th>
-  </tr>
-    <?php
-      try {
-        $dsn = "mysql:host=ws_mysql;dbname=ws_db;";
-        $user = "root";
-        $password = "root";
-        $db = new PDO($dsn, $user, $password);
-      
-        $sql = "SELECT * FROM ws_table";
-        $stt = $db->prepare($sql);
-        $stt->execute();
-        // $result = $stt->fetchAll(PDO::FETCH_ASSOC);
-        // foreach($result as $key => $value) {
-        //   echo $value["id"] . ":" . $value["name"];
-        //   print "<br />";
-        // }
-        while ($result = $stt->fetchAll(PDO::FETCH_ASSOC)) {
-          foreach($result as $value) {
-    ?>
-          <tr>
-            <td><?=$value["id"] ?></td>
-            <td><?=$value["name"] ?></td>
-          </tr>
-    <?php
-          }
-        }
-      } catch (PDOException $e) {
-        echo $e;
-        exit;
-      }
-    ?>
-    </table>
   </body>
 </html>
+
+<?php
+  include_once "./sql_injection.php";
+?>
---
+++ php/src/sql_injection.php
@@ -0,0 +1,36 @@
+<table border="1">
+  <tr>
+    <th>id</th>
+    <th>名前</th>
+  </tr>
+  <?php
+    try {
+      $dsn = "mysql:host=ws_mysql;dbname=ws_db;";
+      $user = "root";
+      $password = "root";
+      $db = new PDO($dsn, $user, $password);
+    
+      $sql = "SELECT * FROM ws_table";
+      $stt = $db->prepare($sql);
+      $stt->execute();
+      // $result = $stt->fetchAll(PDO::FETCH_ASSOC);
+      // foreach($result as $key => $value) {
+      //   echo $value["id"] . ":" . $value["name"];
+      //   print "<br />";
+      // }
+      while ($result = $stt->fetchAll(PDO::FETCH_ASSOC)) {
+        foreach($result as $value) {
+  ?>
+        <tr>
+          <td><?=$value["id"] ?></td>
+          <td><?=$value["name"] ?></td>
+        </tr>
+  <?php
+        }
+      }
+    } catch (PDOException $e) {
+      echo $e;
+      exit;
+    }
+  ?>
+</table>
