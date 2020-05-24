7913ef66ba8a2f1d59904c366c84708f9518cc06
shuta13 <hollystarsun@gmail.com>
2020-05-24 18:13:42 +0900

    feat: DBとの接続完了

---
+++ mysql/init/ws_db.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: ws_db
+-- ------------------------------------------------------
+-- Server version       8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ws_table`
+--
+
+CREATE DATABASE `ws_db`;
+USE `ws_db`;
+
+DROP TABLE IF EXISTS `ws_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ws_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ws_table`
+--
+
+LOCK TABLES `ws_table` WRITE;
+/*!40000 ALTER TABLE `ws_table` DISABLE KEYS */;
+INSERT INTO `ws_table` VALUES (1,'Shuta');
+/*!40000 ALTER TABLE `ws_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-24  9:03:24
--- php/src/index.php
+++ php/src/index.php
@@ -1,2 +1,18 @@
+<meta charset="UTF-8">
+<title>welcome-seminar-2020</title>
 <?php
-phpinfo();
+try {
+  $dsn = "mysql:host=ws_mysql;dbname=ws_db;";
+  $user = "root";
+  $password = "root";
+  $db = new PDO($dsn, $user, $password);
+
+  $sql = "SELECT * FROM ws_table";
+  $s = $db->prepare($sql);
+  $s->execute();
+  $result = $s->fetchAll(PDO::FETCH_ASSOC);
+  var_dump($result);
+} catch (PDOException $e) {
+  echo $e;
+  exit;
+}
