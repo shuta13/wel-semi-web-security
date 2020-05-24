2226e8ceb3e15902640d7f202537033b264146ec
shuta13 <hollystarsun@gmail.com>
2020-05-24 18:37:54 +0900

    wip

--- php/src/index.php
+++ php/src/index.php
@@ -1,21 +1,48 @@
-<meta charset="UTF-8">
-<title>welcome-seminar-2020</title>
 <?php
-try {
-  $dsn = "mysql:host=ws_mysql;dbname=ws_db;";
-  $user = "root";
-  $password = "root";
-  $db = new PDO($dsn, $user, $password);
-
-  $sql = "SELECT * FROM ws_table";
-  $s = $db->prepare($sql);
-  $s->execute();
-  $result = $s->fetchAll(PDO::FETCH_ASSOC);
-
-  foreach($result as $key => $value) {
-    echo $value["id"] . ":" . $value["name"];
-  }
-} catch (PDOException $e) {
-  echo $e;
-  exit;
-}
+declare(strict_types = 1);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>welcome-seminar-2020</title>
+  </head>
+  <body>
+  <table border="1">
+  <tr>
+    <th>id</th>
+    <th>名前</th>
+  </tr>
+    <?php
+      try {
+        $dsn = "mysql:host=ws_mysql;dbname=ws_db;";
+        $user = "root";
+        $password = "root";
+        $db = new PDO($dsn, $user, $password);
+      
+        $sql = "SELECT * FROM ws_table";
+        $stt = $db->prepare($sql);
+        $stt->execute();
+        // $result = $stt->fetchAll(PDO::FETCH_ASSOC);
+        // foreach($result as $key => $value) {
+        //   echo $value["id"] . ":" . $value["name"];
+        //   print "<br />";
+        // }
+        while ($result = $stt->fetchAll(PDO::FETCH_ASSOC)) {
+          foreach($result as $value) {
+    ?>
+          <tr>
+            <td><?=$value["id"] ?></td>
+            <td><?=$value["name"] ?></td>
+          </tr>
+    <?php
+          }
+        }
+      } catch (PDOException $e) {
+        echo $e;
+        exit;
+      }
+    ?>
+    </table>
+  </body>
+</html>
